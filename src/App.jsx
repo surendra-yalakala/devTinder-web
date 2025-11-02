22d9d106f8a595a1aecbf90785a3197d787083f5
Surendra Yalakala <surendrayalakala@gmail.com>
2025-11-02 18:01:46 +0530

    login api integrated, feed, footer, navbar, profile, redux store created

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./Login";
-import Profile from "./Profile";
-import BodyComponent from "./BodyComponent";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import BodyComponent from "./components/BodyComponent";
+import { Provider } from "react-redux";
+import store from "./utils/appStore";
+import Feed from "./components/Feed";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<BodyComponent />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<BodyComponent />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import { addUser } from "../utils/userSlice";
+import { BASE_URL } from "../utils/constants";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("rsharma@gmail.com");
+  const [password, setPassword] = useState("Rohit@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const doLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+
+      dispatch(addUser(res.data));
+      return navigate("/");
+    } catch (error) {
+      console.log("Login ", error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-200 w-96 shadow-sm c">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login!</h2>
+          <div>
+            <fieldset className="fieldset py-2">
+              <legend className="fieldset-legend">Email Id</legend>
+              <input
+                type="text"
+                className="input"
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </fieldset>
+            <fieldset className="fieldset py-2">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="text"
+                className="input"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={() => doLogin()}>
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
