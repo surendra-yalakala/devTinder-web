faa0f8e3f680e7088eb6f53ad464889baa6c064d
Surendra Yalakala <surendrayalakala@gmail.com>
2025-11-03 14:38:31 +0530

    if not auth handled

--- src/components/BodyComponent.jsx
+++ src/components/BodyComponent.jsx
@@ -1,8 +1,37 @@
+import axios from "axios";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const BodyComponent = () => {
+  const dispatch = useDispatch();
+  const userData = useSelector((store) => store.user);
+  const navigate = useNavigate();
+
+  const getUser = async () => {
+    if (userData) return;
+    try {
+      const getUserData = await axios.get(BASE_URL + "profile/view", {
+        withCredentials: true,
+      });
+
+      dispatch(addUser(getUserData));
+    } catch (error) {
+      if (error.status === 401) {
+        navigate("/login");
+      }
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <div>
       <NavBar />
