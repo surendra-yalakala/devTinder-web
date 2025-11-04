3354b690a3f1988e9d3f62171c08e5129a716d8b
Surendra Yalakala <surendrayalakala@gmail.com>
2025-11-04 16:59:51 +0530

    bug: navbar profile img fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,8 +36,11 @@ const NavBar = () => {
             >
               <div className="w-10 rounded-full">
                 <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  alt="user avatar"
+                  src={
+                    user?.photoUrl ||
+                    "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  }
                 />
               </div>
             </div>
