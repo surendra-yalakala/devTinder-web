6bde2e57a36fa06696468340b7a283b6649a3c5c
Surendra Yalakala <surendrayalakala@gmail.com>
2025-11-04 07:17:16 +0530

    feat:edit profile created

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,21 +1,21 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import UserCard from "./UserCard";
+import { BASE_URL } from "../utils/constants";
+import { addUser } from "../utils/userSlice";
 
-const EditProfile = () => {
-  const user = useSelector((store) => store.user);
-  const [firstName, setFirstName] = useState(user.firstName);
-  const [lastName, setLastName] = useState(user.lastName);
-  const [age, setAge] = useState(user.age);
-  const [gender, setGender] = useState(user.gender);
-  const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
-  const [about, setAbout] = useState(user.about);
+const EditProfile = ({ user }) => {
+  const [firstName, setFirstName] = useState(user?.firstName);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [age, setAge] = useState(user?.age);
+  const [gender, setGender] = useState(user?.gender);
+  const [photoUrl, setPhotoUrl] = useState(user?.photoUrl);
+  const [about, setAbout] = useState(user?.about);
   const [errorMsg, setErrorMsg] = useState("");
+  const [showToast, setShowToast] = useState(false);
 
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   const saveProfile = async () => {
     try {
@@ -31,87 +31,107 @@ const EditProfile = () => {
         },
         { withCredentials: true }
       );
+      console.log("save profile ", res?.data?.data);
 
-      //   dispatch(addUser(res.data));
-      //   return navigate("/");
+      dispatch(addUser(res?.data?.data));
+      setShowToast(true);
+      setTimeout(() => {
+        setShowToast(false);
+      }, 2000);
     } catch (error) {
-      setErrorMsg(error.response.data.message);
+      console.log(error);
+
+      //   setErrorMsg(error.response.data);
     }
   };
 
   return (
-    <div className="flex justify-center items-start gap-10 my-10 flex-wrap">
-      <div className="flex justify-center my-10">
-        <div className="card bg-base-200 w-96 shadow-sm">
-          <div className="card-body">
-            <h2 className="card-title justify-center">Profile</h2>
-            <div>
-              <fieldset className="fieldset py-2">
-                <legend className="fieldset-legend">First Name</legend>
-                <input
-                  type="text"
-                  className="input"
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                />
-              </fieldset>
-              <fieldset className="fieldset py-2">
-                <legend className="fieldset-legend">Last Name</legend>
-                <input
-                  type="text"
-                  className="input"
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                />
-              </fieldset>
-              <fieldset className="fieldset py-2">
-                <legend className="fieldset-legend">Age</legend>
-                <input
-                  type="text"
-                  className="input"
-                  value={age}
-                  onChange={(e) => setAge(e.target.value)}
-                />
-              </fieldset>
-              <fieldset className="fieldset py-2">
-                <legend className="fieldset-legend">Gender</legend>
-                <input
-                  type="text"
-                  className="input"
-                  value={gender}
-                  onChange={(e) => setGender(e.target.value)}
-                />
-              </fieldset>
-              <fieldset className="fieldset py-2">
-                <legend className="fieldset-legend">Photo Url</legend>
-                <input
-                  type="text"
-                  className="input"
-                  value={photoUrl}
-                  onChange={(e) => setPhotoUrl(e.target.value)}
-                />
-              </fieldset>
-              <fieldset className="fieldset py-2">
-                <legend className="fieldset-legend">About</legend>
-                <input
-                  type="text"
-                  className="input"
-                  value={about}
-                  onChange={(e) => setAbout(e.target.value)}
-                />
-              </fieldset>
-            </div>
-            <p className="text-red-500">{errorMsg}</p>
-            <div className="card-actions justify-center">
-              <button className="btn btn-primary" onClick={() => saveProfile()}>
-                Save Profile
-              </button>
+    <>
+      <div className="flex justify-center items-start gap-10 my-10 flex-wrap">
+        <div className="flex justify-center my-10">
+          <div className="card bg-base-200 w-96 shadow-sm">
+            <div className="card-body">
+              <h2 className="card-title justify-center">Profile</h2>
+              <div>
+                <fieldset className="fieldset py-2">
+                  <legend className="fieldset-legend">First Name</legend>
+                  <input
+                    type="text"
+                    className="input"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="fieldset py-2">
+                  <legend className="fieldset-legend">Last Name</legend>
+                  <input
+                    type="text"
+                    className="input"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="fieldset py-2">
+                  <legend className="fieldset-legend">Age</legend>
+                  <input
+                    type="text"
+                    className="input"
+                    value={age}
+                    onChange={(e) => setAge(e.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="fieldset py-2">
+                  <legend className="fieldset-legend">Gender</legend>
+                  <input
+                    type="text"
+                    className="input"
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="fieldset py-2">
+                  <legend className="fieldset-legend">Photo Url</legend>
+                  <input
+                    type="text"
+                    className="input"
+                    value={photoUrl}
+                    onChange={(e) => setPhotoUrl(e.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="fieldset py-2">
+                  <legend className="fieldset-legend">About</legend>
+                  <input
+                    type="text"
+                    className="input"
+                    value={about}
+                    onChange={(e) => setAbout(e.target.value)}
+                  />
+                </fieldset>
+              </div>
+              <p className="text-red-500">{errorMsg}</p>
+              <div className="card-actions justify-center">
+                <button
+                  className="btn btn-primary"
+                  onClick={() => saveProfile()}
+                >
+                  Save Profile
+                </button>
+              </div>
             </div>
           </div>
         </div>
+        <UserCard
+          user={{ firstName, lastName, photoUrl, age, gender, about }}
+        />
       </div>
-      <UserCard data={user} />
-    </div>
+      {showToast && (
+        <div className="toast toast-top toast-center">
+          <div className="alert alert-success">
+            <span>Profile saved successfully.</span>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 
-const UserCard = (props) => {
+const UserCard = ({ user }) => {
+  const { firstName, lastName, photoUrl, age, gender, about } = user;
+
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="card bg-base-300 w-96 shadow-sm">
+      <div className="card bg-base-300 w-96 shadow-xl">
         <figure>
-          <img src={props?.data?.photoUrl} alt="user" />
+          <img src={photoUrl} alt="user" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">
-            {props?.data?.firstName + " " + props?.data?.lastName}
-          </h2>
-          {props?.data?.gender && props?.data?.age && (
-            <p>{props?.data?.gender + ", " + props?.data?.age}</p>
-          )}
-          <p>{props?.data?.about}</p>
-          <div className="card-actions justify-end">
+          <h2 className="card-title">{firstName + " " + lastName}</h2>
+          {gender && age && <p>{gender + ", " + age}</p>}
+          <p>{about}</p>
+          <div className="card-actions justify-end my-4">
             <button className="btn btn-primary">Ignore</button>
             <button className="btn btn-secondary">Interested</button>
           </div>
