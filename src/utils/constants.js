bf6d0ae7b14a4130ffa1509e553d9c89ea548cfa
Surendra Yalakala <surendrayalakala@gmail.com>
2025-11-07 19:48:09 +0530

    made base url dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "/api";
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
 
 export const TOAST_DURATION = 2000;
 export const TOAST_TYPES = {
