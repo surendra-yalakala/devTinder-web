cd179bf9042729396e247287eaf2503d1f396078
Surendra Yalakala <surendrayalakala@gmail.com>
2025-11-11 07:17:28 +0530

    feat:premium screen created

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -61,6 +61,9 @@ const NavBar = () => {
                 <Link to="/requests">Requests</Link>
               </li>
               <li>
+                <Link to="/premium">Premium</Link>
+              </li>
+              <li>
                 <a onClick={() => doLogout()}>Logout</a>
               </li>
             </ul>
